9d988a4d9c5a1cdc6c455de62e1d66b3c0f2d518
DevolperPHP <devolperphp@gmail.com>
2016-09-05 21:56:36 +0300

    Update clearchatdp.php

--- src/clearchatdp.php
+++ src/clearchatdp.php
@@ -1,2 +1,137 @@
-# ClearChatDP
-Commads
+<?php
+
+namespace ClearChatDP;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\utils\TextFormat as Color;
+
+class clearchatdp extends PluginBase{
+	
+	public function onEnable(){
+		$this->getServer()->getLogger()->info(Color::GREEN."[ClearChat] Plugin Has Been Enabled");
+	}
+	
+	public function onDisable(){
+		$this->getServer()->getLogger()->info(Color::RED."[ClearChst] Plugin Has Been Disabled");
+	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		switch($cmd->getName()){
+			case 'cdp':
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."                                                                                  ");
+				$sender->sendMessage(Color::GREEN."Chat Has Been Clend");
